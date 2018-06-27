2584d6052d5cef81876e9ee08500ede55a800095
ximuli <ximuli666@163.com>
2018-06-27 21:57:08 +0800

    无缝轮播初实现

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$('.images > img:nth-child(1)').addClass('current')
+$('.images > img:nth-child(2)').addClass('enter')
+$('.images > img:nth-child(3)').addClass('enter')
+
+let n = 1;
+
+function x(n) {
+	if ( n > 3) {
+		n = n % 3
+		if (n === 0) {
+			n = 3
+		}
+	}   
+	return n //得到一个循环等于 1 2 3的 n
+}
+
+setInterval( ()=>{
+	$(`.images > img:nth-child(${x(n)}`).removeClass('current').addClass('leave')
+	  .one('transitionend', (e)=>{$(e.currentTarget).removeClass('leave').addClass('enter')})
+
+	$(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+
+	n += 1
+}, 2000)
+
+
+
+
