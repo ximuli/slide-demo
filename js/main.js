029b8266e9b3d1001526a06d36d0c7d54b02aa8a
ximuli <ximuli666@163.com>
2018-08-08 21:01:04 +0800

    新思路实现无缝轮播

--- js/main.js
+++ js/main.js
@@ -1,53 +1,67 @@
-let n
-初始化()
+let $buttons = $('#buttonWrapper > button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
 
-setInterval( ()=>{
-	makeLeave(getImage(n))
-	  .one('transitionend', (e)=>{
-	  	makeEnter($(e.currentTarget))
-	  })
-	makeCurrent(getImage(n+1))
-	n += 1
-}, 3000)
+makeFakeSlides()
+$slides.css({transform: 'translateX(-400px)'})
+bindEvents()
 
 
 
+function bindEvents() {
+	$buttons.eq(0).on('click', function() {
+	
+	if (current === 2) {
+		console.log('说明是从最后一张到第一张')
+		$slides.css({transform: 'translateX(-1600px)'})
+		    .one('transitionend', function() {
+		    	$slides.hide()
+		    	  .offset()
+		    	$slides.css({transform:'translateX(-400px)'})
+		    	  .show()
+		    })
+	}
+	else {
+		$slides.css({transform: 'translateX(-400px)'})
+	}
+
+	current = 0
+	
+})
+
+$buttons.eq(1).on('click', function() {
+	$slides.css({
+		transform: 'translateX(-800px)'
+	})
+	current = 1
+})
+
+$buttons.eq(2).on('click', function() {
+	if (current === 0) {
+		console.log('说明是从第一张到最后一张')
+		$slides.css({transform: 'translateX(0)'})
+		    .one('transitionend', function() {
+		    	$slides.hide()
+		    	  .offset()
+		    	$slides.css({transform:'translateX(-1200px)'})
+		    	  .show()
+
+		    })
+	}
+	else {
+		$slides.css({transform: 'translateX(-1200px)'})
+	}
+	current = 2
+})
 
 
-
-//  下面的函数可以不用看
-
-function getImage(n) {
-	return $(`.images > img:nth-child(${x(n)}`)
 }
 
-function x(n) {
-	if ( n > 3) {
-		n = n % 3
-		if (n === 0) {
-			n = 3
-		}
-	}   
-	return n //得到一个循环等于 1 2 3的 n
-}
 
-function 初始化() {
-	n = 1;
-	$(`.images > img:nth-child(${n})`).addClass('current')
-	  .siblings().addClass('enter')
-}
-
-function makeCurrent($node) {
-	$node.removeClass('enter').addClass('current')
-	return $node
-}
-
-function makeLeave($node) {
-	$node.removeClass('current').addClass('leave')
-	return $node
-}
-
-function makeEnter($node) {
-	$node.removeClass('leave').addClass('enter')
-	return $node
+function makeFakeSlides() {
+	let $firstCopy = $images.eq(0).clone(true)
+    let $lastCopy = $images.eq($images.length-1).clone(true)
+    $slides.append($firstCopy)
+    $slides.prepend($lastCopy)
 }
