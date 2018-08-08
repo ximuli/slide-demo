c3a2534931e19a82dfebd967827fc269a1764305
ximuli <ximuli666@163.com>
2018-08-08 22:44:01 +0800

    优化代码结构，添加功能。

--- js/main.js
+++ js/main.js
@@ -7,57 +7,64 @@ makeFakeSlides()
 $slides.css({transform: 'translateX(-400px)'})
 bindEvents()
 
+$('#next').on('click', function() {
+	goToSlide(current+1)
+})
+
+$('#previous').on('click', function() {
+	goToSlide(current-1)
+})
+
+let timerId = setInterval(()=>{
+	goToSlide(current+1)
+},2000)
+
+$('.container').on('mouseenter', function() {
+	window.clearInterval(timerId)
+}).on('mouseleave', function() {
+	timerId = setInterval(()=>{
+		goToSlide(current+1)
+	},2000)
+})
 
 
 function bindEvents() {
-	$buttons.eq(0).on('click', function() {
-	
-	if (current === 2) {
-		console.log('说明是从最后一张到第一张')
-		$slides.css({transform: 'translateX(-1600px)'})
-		    .one('transitionend', function() {
-		    	$slides.hide()
-		    	  .offset()
-		    	$slides.css({transform:'translateX(-400px)'})
-		    	  .show()
-		    })
-	}
-	else {
-		$slides.css({transform: 'translateX(-400px)'})
-	}
-
-	current = 0
-	
-})
-
-$buttons.eq(1).on('click', function() {
-	$slides.css({
-		transform: 'translateX(-800px)'
-	})
-	current = 1
-})
-
-$buttons.eq(2).on('click', function() {
-	if (current === 0) {
-		console.log('说明是从第一张到最后一张')
-		$slides.css({transform: 'translateX(0)'})
-		    .one('transitionend', function() {
-		    	$slides.hide()
-		    	  .offset()
-		    	$slides.css({transform:'translateX(-1200px)'})
-		    	  .show()
-
-		    })
-	}
-	else {
-		$slides.css({transform: 'translateX(-1200px)'})
-	}
-	current = 2
-})
-
-
+  $('#buttonWrapper').on('click', 'button', function(e) {
+	let $button = $(e.currentTarget)
+	let index = $button.index()
+	goToSlide(index)
+  })
 }
 
+function goToSlide(index) {
+    if (index > $buttons.length-1) {
+    	index = 0
+    }
+    else if (index < 0) {
+        index = $buttons.length -1
+    }
+
+	if ( current === $buttons.length-1 && index === 0) {
+        //最后一张到第一张
+        $slides.css({transform: `translateX(${- ($buttons.length + 1) *400}px)`})
+		    .one('transitionend', function() {
+		    	$slides.hide().offset()
+		    	$slides.css({transform:`translateX(${- (index+1) * 400}px)`}).show()
+		    })
+	} else if ( current === 0 && index === $buttons.length -1 ) {
+        //第一张到最后一张
+        $slides.css({transform: `translateX(0px)`})
+		    .one('transitionend', function() {
+		    	$slides.hide().offset()
+		    	$slides.css({transform:`translateX(${- (index+1) * 400}px)`}).show()
+		    })
+	}
+	else {
+		$slides.css({transform:`translateX(${- (index+1) * 400}px)`})
+	}
+	
+	current = index 
+}
 
 function makeFakeSlides() {
 	let $firstCopy = $images.eq(0).clone(true)
